13c133dade1d82e1a9a1d3ea3665ac9b9837d31a
Snagjun Kim <Sangjun.Kim.dev@gmail.com>
2022-08-22 13:20:58 +0900

    single_pages 수정 및 생성

--- single_pages/apps.py
+++ single_pages/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SinglePasgesConfig(AppConfig):
+class SinglePagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'single_pasges'
+    name = 'single_pages'
