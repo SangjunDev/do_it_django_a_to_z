55c143339a3bcac283ef5dfcb583d5a4519ef1c6
Snagjun Kim <Sangjun.Kim.dev@gmail.com>
2022-08-25 15:39:42 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Tag(models.Model):
+  name = models.CharField(max_length = 50, unique=True)
+  slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+      return f'/blog/tag/{self.slug}/'
+
 class Category(models.Model):
   name = models.CharField(max_length = 50, unique=True)
   slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -16,8 +26,6 @@ class Category(models.Model):
     
   def get_absolute_url(self):
       return f'/blog/category/{self.slug}/'
-    
-    
     
 
 class Post(models.Model):
@@ -36,6 +44,8 @@ class Post(models.Model):
   
   category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
   
+  tags = models.ManyToManyField(Tag, blank=True)
+  
   def __str__(self):
     return f'[{self.pk}]{self.title} :: {self.author}'
 
@@ -48,7 +58,8 @@ class Post(models.Model):
   def get_file_ext(self):
     return self.get_file_name().split('.')[-1]
   
-    
+
+
   
 
   
