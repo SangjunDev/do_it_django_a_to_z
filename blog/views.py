03dd23801f3a878b4473a5190fb0b629a82dc78e
Snagjun Kim <Sangjun.Kim.dev@gmail.com>
2022-08-29 14:03:32 +0900

    댓글 삭제 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+import re
 from tkinter.tix import Tree
 from urllib import request, response
 from django.shortcuts import render, redirect
@@ -171,6 +172,15 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
-            raise PermissionDenied     
+            raise PermissionDenied
+          
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied          
   
         
