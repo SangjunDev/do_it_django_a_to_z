a25a77865df5b55257197a5f2066e824f45bab72
Snagjun Kim <Sangjun.Kim.dev@gmail.com>
2022-08-29 10:30:48 +0900

    댓륵 폼으로 포스트 상세 페이지에서 댓글 작성하기

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from tkinter.tix import Tree
-from urllib import response
+from urllib import request, response
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
 from .models import Post , Category, Tag
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 
@@ -27,6 +29,7 @@ class PostDetail(DetailView):
       context = super(PostDetail, self).get_context_data()
       context['categories'] = Category.objects.all()
       context['no_category_post_count'] = Post.objects.filter(category=None).count()
+      context['comment_form'] = CommentForm
       return context
 
 class PostCreate(LoginRequiredMixin,UserPassesTestMixin, CreateView):
@@ -105,10 +108,6 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
         return response      
         
-      
-        
-
-
 def category_page(request, slug):
   if slug == 'no_category':
     category = '미분류'
@@ -144,5 +143,24 @@ def tag_page(request, slug):
       
     }
   )
+  
+def new_comment(request, pk):
+  if request.user.is_authenticated:
+    post = get_object_or_404(Post, pk=pk)
+    
+    if request.method =='POST' :
+      comment_form = CommentForm(request.POST)
+      if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.post = post
+        comment.author = request.user
+        comment.save()
+        return redirect(comment.get_absolute_url())
+      
+    else :
+      return redirect(post.get_absolute_url())
+    
+  else:
+    raise PermissionDenied      
   
         
